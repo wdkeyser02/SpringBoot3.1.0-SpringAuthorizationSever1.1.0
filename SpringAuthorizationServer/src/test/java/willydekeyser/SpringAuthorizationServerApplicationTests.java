0e766c504ba5a2e464fa35edb5c5aeb0acb3c8fe
willydekeyser <ma074172@skynet.be>
2023-05-31 14:44:38 +0200

    Setup Spring Authorization Server

---
+++ SpringAuthorizationServer/src/test/java/willydekeyser/SpringAuthorizationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package willydekeyser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAuthorizationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
