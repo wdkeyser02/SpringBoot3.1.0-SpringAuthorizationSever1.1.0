6c56fff0d28094c42b21de0bd137fc681f7ad4fb
willydekeyser <ma074172@skynet.be>
2023-06-08 18:36:37 +0200

    Update Login

--- SpringOAuth2Client/src/main/java/willydekeyser/controller/DefaultController.java
+++ SpringOAuth2Client/src/main/java/willydekeyser/controller/DefaultController.java
@@ -1,5 +1,6 @@
 package willydekeyser.controller;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,15 +14,15 @@ public class DefaultController {
 	}
 
 	@GetMapping("/index")
-	public String index(Model model) {
-		model.addAttribute("login", false);
+	public String index(Model model, Authentication authentication) {
+		model.addAttribute("login", authentication);
 		return "index";
 	}
 
 	@GetMapping("/logged-out")
-	public String loggedOut(Model model) {
-		model.addAttribute("login", false);
-		return "logged-out";
+	public String loggedOut(Model model, Authentication authentication) {
+		model.addAttribute("login", authentication);
+		return "index";
 	}
 
 }
